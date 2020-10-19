4f1db8fe3d2f4dfe04f463f70824b3daae3f4487
TerenceSimplon <desdi0406>
2020-10-19 19:04:45 +0200

    corrections fond d'ecran accueil

--- application/views/home.php
+++ application/views/home.php
@@ -1,32 +1,33 @@
 
-<div class="container pt-5 accueil">
+<div class="container accueil">
+	<div class="pt-5">
+		<!--  couverture + résumé  -->
+		<div class="accueil__haut mb-lg-5 ">
+			<div class="row">
+				<div class="col-12 col-lg-6 offset-lg-1 text-center">
+					<img src="<?= base_url("/resources/img/couverture_territoires_solidaires.png") ?>" alt="Couverture du livre" class=" mb-3 border border-dark couverture">
+				</div>
 
-	<!--  couverture + résumé  -->
-	<div class="container accueil__haut mb-lg-5 ">
-		<div class="row">
-			<div class="col-12 col-lg-6 offset-lg-1 text-center">
-				<img src="<?= base_url("/resources/img/couverture_territoires_solidaires.png") ?>" alt="Couverture du livre" class=" mb-3 border border-dark couverture">
-			</div>
+				<div class="col-12 col-lg-5 text-center align-self-center d-lg-flex flex-column  align-items-lg-center">
+					<button type="button " class="btn bouton__livre btn-success order-lg-2"><a class="lien-edition"  href="http://www.editionsatelier.com/index.php?page=shop.product_details&flypage=bookshop-flypage.tpl&product_id=816&category_id=47&manufacturer_id=1&option=com_virtuemart&Itemid=1" target="_blank">Acheter sur Les Éditions de l'Atelier</a></button>
+					<p class="p-3 descriptif__livre order-lg-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis cum accusantium voluptatum labore? Eaque tempora impedit, omnis, blanditiis sint delectus maiores laborum, consectetur similique nihil repellendus rerum animi tempore vitae
+						sint delectus maiores laborum, consectetur similique nihil repellendus rerum animi tempore vita. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis cum accusantium voluptatum labore? Eaque tempora impedit, omnis, blanditiis sint delectus maiores laborum, consectetur similique nihil repellendus rerum animi tempore vitae
+						sint delectus maiores laborum, consectetur similique nihil repellendus rerum animi tempore vita. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis cum accusantium voluptatum labore? Eaque tempora impedit, omnis, blanditiis sint delectus maiores laborum, consectetur similique nihil repellendus rerum animi tempore vitae
+						sint delectus maiores laborum, consectetur similique nihil repellendus rerum animi tempore vita.</p>
 
-			<div class="col-12 col-lg-5 text-center align-self-center d-lg-flex flex-column  align-items-lg-center">
-				<button type="button " class="btn bouton__livre btn-success order-lg-2"><a class="lien-edition"  href="http://www.editionsatelier.com/index.php?page=shop.product_details&flypage=bookshop-flypage.tpl&product_id=816&category_id=47&manufacturer_id=1&option=com_virtuemart&Itemid=1" target="_blank">Acheter sur Les Éditions de l'Atelier</a></button>
-				<p class="p-3 descriptif__livre order-lg-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis cum accusantium voluptatum labore? Eaque tempora impedit, omnis, blanditiis sint delectus maiores laborum, consectetur similique nihil repellendus rerum animi tempore vitae
-					sint delectus maiores laborum, consectetur similique nihil repellendus rerum animi tempore vita. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis cum accusantium voluptatum labore? Eaque tempora impedit, omnis, blanditiis sint delectus maiores laborum, consectetur similique nihil repellendus rerum animi tempore vitae
-					sint delectus maiores laborum, consectetur similique nihil repellendus rerum animi tempore vita. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis cum accusantium voluptatum labore? Eaque tempora impedit, omnis, blanditiis sint delectus maiores laborum, consectetur similique nihil repellendus rerum animi tempore vitae
-					sint delectus maiores laborum, consectetur similique nihil repellendus rerum animi tempore vita.</p>
-
-					<div class="col-12 order-lg-4 mt-3" >
-						<a href="<?= base_url("/thematique/01-elements-de-mode-demploi") ?>"><button type="button" class="btn btn-secondary w-lg-50 w-100 mb-3 py-2">Modes d'emploi des conférences</button></a>
-					</div>
-					<div class="col-12 order-lg-5">
-						<a href="<?= base_url("/timeline") ?>"><button type="button " class="btn btn-secondary w-lg-50 w-100 mb-3 py-2">Déroulé de l'événement</button></a>
-					</div>
-					<div class="col-12 order-lg-6">
-						<a href="<?= base_url("/thematiques") ?>"><button type="button " class="btn btn-secondary w-lg-50 w-100 mb-3 py-2">Thématiques abordées</button></a>
-					</div>
+						<div class="col-12 order-lg-4 mt-3" >
+							<a href="<?= base_url("/thematique/01-elements-de-mode-demploi") ?>"><button type="button" class="btn btn-primary w-lg-50 w-100 mb-3 py-2">Modes d'emploi des conférences</button></a>
+						</div>
+						<div class="col-12 order-lg-5">
+							<a href="<?= base_url("/timeline") ?>"><button type="button " class="btn btn-primary w-lg-50 w-100 mb-3 py-2">Déroulé de l'événement</button></a>
+						</div>
+						<div class="col-12 order-lg-6">
+							<a href="<?= base_url("/thematiques") ?>"><button type="button " class="btn btn-primary w-lg-50 w-100 mb-3 py-2">Thématiques abordées</button></a>
+						</div>
 
 
 
+				</div>
 			</div>
 		</div>
 	</div>
