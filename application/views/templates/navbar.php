a425aa8f485df821af6fd53432a0f92ee6121c24
Mickael-DP <m.dallepasqualine@gmail.com>
2020-10-20 15:36:40 +0200

    Ajout accueil dans navbar

--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -1,9 +1,9 @@
 <header> 
     <div>
         <nav class="navbar w-100 d-flex flex-row justify-content-between">
-            <a href="<?= base_url("/") ?>"><img class="img-cerisy" src="<?= base_url("/resources/img/img-1.jpg") ?>" alt="Les colloques CERISY"></a>
-                <span class="h6 d-none d-lg-block text-uppercase font-weight-bold">Territoire solidaire en commun</span>
-                <button type="button" class="btn btn-success">
+            <button type="button" class="btn btn-light" href=""><i class="fas fa-home icon-house"></i>Accueil</button>
+            <span class="h6 d-none d-lg-block text-uppercase font-weight-bold">Territoire solidaire en commun</span>
+            <button type="button" class="btn btn-success">
                 <a class= "lien-edition" href="http://www.editionsatelier.com/index.php?page=shop.product_details&flypage=bookshop-flypage.tpl&product_id=816&category_id=47&manufacturer_id=1&option=com_virtuemart&Itemid=1" >Acheter le livre</a></button>
             <form class="form-inline d-flex flex-row flex-nowrap" action="<?= base_url("/rechercher") ?>" method="GET">
                 <p class="title-search d-none d-sm-block">Recherche</p>
