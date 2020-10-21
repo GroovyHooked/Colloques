88ded3e225941b1b61d25c4e7aa6994bbd94767b
unknown <krystopher.logez@hotmail.fr>
2020-10-21 11:01:59 +0200

    Ajout de la card intervenant; fix de la barre vertical de la page timeline sur safari (à tester)

---
+++ application/views/templates/card-intervenant.php
@@ -0,0 +1,9 @@
+<div class="card my-2 card-custom">
+    <div style="background-image: url(<?= 'IMAGE ICI' ?>)" alt="<?= 'ALT ICI' ?>"
+        class="card-img card-custom-img card-custom-img-intervenant"></div>
+    <div class="card-body pb-2">
+        <h5 class="card-title mb-2 font-weight-bold text-center">
+            <a href="#"><?= 'NOM INTERVENANT ICI' ?></a>
+        </h5>
+    </div>
+</div>
