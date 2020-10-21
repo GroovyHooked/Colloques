045042198f4cf6d719ce4d4dd8fc927122fe25d5
astroware <terence0406>
2020-10-21 11:25:03 +0200

    correction image de fond

--- application/views/intervenants.php
+++ application/views/intervenants.php
@@ -1,20 +1,27 @@
 <div class="container my-5">
 	<div class="row">
 		<div class="col-12 my-4">
-			<h1 class="title h2 my-2 ml-2 mb-0 custom-title">Intervenants</h1>
+			<h1 class="title h2 my-2 ml-2 mb-0 custom-title">Présentation des intervenants</h1>
 		</div>
 	</div>
-	<ul>
+	<div class="row">
 	<?php
 
 	foreach ($intervenants as $key => $value)
 	{
 	?>
-		<li><a href="<?= base_url("/intervenant/".$key) ?>"><?= $value ?></a></li>
+		<div class="col-12 col-md-6 col-lg-4 col-xl-3 ressource-card">
+			<?php
+			$datas = array();
+			$datas["nom_intervenant"] = $value ;
+			$datas["id_intervenant"] = $key ;
+			$this->load->view('templates/card-intervenant', $datas);
+			?>
+		</div>
 	<?php
 	}
 	?>
-	</ul>
+	</div>
 </div>
 
 
--- application/views/templates/card-intervenant.php
+++ application/views/templates/card-intervenant.php
@@ -1,9 +1,13 @@
 <div class="card my-2 card-custom">
-    <div style="background-image: url(<?= 'IMAGE ICI' ?>)" alt="<?= 'ALT ICI' ?>"
-        class="card-img card-custom-img card-custom-img-intervenant"></div>
+	<a href="<?= base_url("/intervenant/".$id_intervenant) ?>">
+		<div style="background-image: url(<?= $url_resources. "img/intervenants/".$id_intervenant.".JPG" ?>)" alt="<?= $nom_intervenant ?>"
+			class="card-img card-custom-img card-custom-img-intervenant">
+
+		</div>
+	</a>
     <div class="card-body pb-2">
         <h5 class="card-title mb-2 font-weight-bold text-center">
-            <a href="#"><?= 'NOM INTERVENANT ICI' ?></a>
+            <a href="<?= base_url("/intervenant/".$id_intervenant) ?>"><?= $nom_intervenant  ?></a>
         </h5>
     </div>
 </div>
