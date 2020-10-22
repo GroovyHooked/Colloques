394bea56238c643cbc7e21293da44c65a6c77c72
astroware <terence0406>
2020-10-22 15:19:30 +0200

    Page d'accueil

--- application/views/home.php
+++ application/views/home.php
@@ -6,6 +6,9 @@
 			<div class="row">
 				<div class="col-12 col-lg-6 offset-lg-1 text-center">
 					<img src="<?= base_url("/resources/img/couverture_territoires_solidaires.png") ?>" alt="Couverture du livre" class=" mb-3 border border-dark couverture">
+					<div class="col-12 text-center">
+						<button type="button" class="btn btn-success" class="savoirPlus">En savoir plus</button>
+					</div>
 				</div>
 
 				<div class="col-12 col-lg-5 text-center align-self-center d-lg-flex flex-column  align-items-lg-center">
@@ -15,8 +18,23 @@
 							<button type="button " class="w-100 btn bouton__livre btn-success order-lg-2"><a class="lien-edition"  href="http://www.editionsatelier.com/index.php?page=shop.product_details&flypage=bookshop-flypage.tpl&product_id=816&category_id=47&manufacturer_id=1&option=com_virtuemart&Itemid=1" target="_blank">Acheter sur le site des Éditions de l'Atelier</a></button>
 						</div>-->
 						<div class="col-12 p-3 descriptif__livre order-lg-1">
+							<p>Dès sa préparation, nous avions anticipé que la valorisation du colloque « Territoires solidaires en commun : controverse à l’horizon du translocalisme » qui s’est déroulé au Centre Culturel de Cerisy du 12 au 19 juillet
+								2019, ne prendrait pas la forme classique d’actes de colloque.</p>
+							<span class="texte__cache">En effet, ce colloque s’est volontairement basé sur des tables rondes (TR) dans lesquelles des actrices et des acteurs ont été invité·e·sà nous dire quel territoire leurs actions collectives font localement émerger, comment celles-ci construisent des solidarités autour des ressources mises ou produites en commun et, enfin, quels liens les relient à d’autres échelles.</span>
+							<p>Le matériau de base de notre colloque n’est donc pas constitué de textes mais de récits d’expériences autour d’une trame commune.</p> <span class="texte__cache">Le pari du colloque fut que, sur cette base, via l’entrecroisement multiple de ces récits et de leur mise en perspective lors des moments forts du colloque (les débats publics (DP), les conférences plénières (PL), les tables de controverses), un nouvel horizon se lève sur les communs à même d’ouvrir la voie à une transformation globale de nos sociétés. Tout au long du colloque, pour pouvoir mieux cerner le translocalisme, il nous fallait réussir à identifier les liens, dans leur diversité, qui s’exprimeraient ou apparaîtraient dans cet entrecroisement de récits et de réflexions. Nous avons ainsi sollicité l’aide de personnes présentes sur toute la durée du colloque
+                                en leur confiant un rôle de « capteurs de liens ».</span>
+							<p>Pour réfléchir aux contenus et à la forme de ces « anti-actes du colloque », nous nous sommes associés aux Éditions de l’Atelier et nous avons abouti à une architecture qui associe deux espaces, le livre papier (que nous
+								vous invitons à découvrir également) et ce site compagnon, reliés par un fil commun.</p>
+							<span class="texte__cache">En effet, chacun d’eux propose de raconter des voyages vers l’horizon translocaliste des territoires solidaires en commun. Quoique de manière différente.</span>
+							<p>Dans le livre, nous proposons des récits de voyages et des poèmes de Jacques Jouet qui racontent comment, ces sept jours du 12 au 19 juillet 2019, ont fait événement. Chacun de ces récits renvoie à ce site internet compagnon,
+								qui vous offre la possibilité de construire votre propre voyage en traçant votre propre itinéraire singulier au pays du translocalisme des communs sur la base d’une architecture mettant en scène l’ensemble des matériaux
+								de ce colloque. Vous pouvez suivre le déroulé chronologique du colloque et faire votre choix d’escales parmi l’ensemble des tables rondes (TR) ou des débats publics (DP). Vous pouvez aussi suivre la pensée d’un·e intervenant·e
+								en particulier. Mais vous avez également la possibilité de partir à la découverte des thématiques abordées</p><span class="texte__cache"> : découvrir les moments où les quatre vecteurs du translocalisme que nous avions posés en hypothèse (la démocratie, la coopération, le numérique et l’écologie), ou bien de vous laisser porter par la matrice de questionnements, de liens et d’idées qui ont été repérés, tout au long du colloque et dans les mois qui ont suivi, par les « capteurs de liens », les coorganisateurs et l’équipe des Éditions de l’Atelier. Ces liens et idées sont susceptibles de permettre à chacun·e de construire ses chemins vers la société des communs.</span>
+							<p>Et partout, vous pourrez faire des rencontres et vous arrêter pour découvrir l’une des créations spontanées réalisées pendant le colloque</p><span class="texte__cache"> (les poèmes, la chanson La Translocale, les annotations post-it, les étonnements, etc.)</span>
+							<p>et manifestant la place essentielle du sensible dans les thèses abordées et les transformations recherchées.</p>
+							<span class="texte__cache">En cohérence avec l’approche des communs, ce site compagnon donne ainsi accès libre aux ressources produites par l’ensemble des participant·e·s avant, durant et après ce colloque.</span>
 
-							Texte à remplacer
+							<p>Bon voyage et n’hésitez pas à nous en faire, à votre tour, le récit !</p>
 
 						</div>
 
@@ -41,8 +59,15 @@
 	</div>
 
 
-	<!-- Liens mode d'emploi / Timeline / Thématique  -->
+	<script>
+		$(document).ready(function() {
+			$("button, .descriptif__livre").click(function() {
+				$("span").toggleClass("texte__cache");
 
+
+			})
+		})
+	</script>
 
 </div>
 
