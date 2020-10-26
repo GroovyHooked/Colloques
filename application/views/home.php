617115272b80e3aa38fae4ab0a24c4ef2668faa8
astroware <terence0406>
2020-10-26 13:56:05 +0100

    nouvelles maj thématiques + home

--- application/views/home.php
+++ application/views/home.php
@@ -16,41 +16,41 @@
 							<button type="button " class="w-100 btn bouton__livre btn-success order-lg-2"><a class="lien-edition"  href="http://www.editionsatelier.com/index.php?page=shop.product_details&flypage=bookshop-flypage.tpl&product_id=816&category_id=47&manufacturer_id=1&option=com_virtuemart&Itemid=1" target="_blank">Acheter sur le site des Éditions de l'Atelier</a></button>
 						</div>-->
 						<div class="col-12 p-3 descriptif__livre order-lg-1">
-							<p>Dès sa préparation, nous avions anticipé que la valorisation du colloque « Territoires solidaires en commun : controverse à l’horizon du translocalisme » qui s’est déroulé au Centre Culturel de Cerisy du 12 au 19 juillet
-								2019, ne prendrait pas la forme classique d’actes de colloque.
 
-								<span class="texte__cache">En effet, ce colloque s’est volontairement basé sur des tables rondes (TR)
-								dans lesquelles des actrices et des acteurs ont été invité·e·sà nous dire quel territoire
-								leurs actions collectives font localement émerger, comment celles-ci construisent des solidarités
-								autour des ressources mises ou produites en commun et, enfin, quels liens les relient à d’autres échelles.</span>
+							<p>
+								La valorisation du colloque «Territoires solidaires en commun : controverse à l’horizon du translocalisme », qui s’est déroulé au Centre culturel de Cerisy du 12 au 19 juillet 2019 ne pouvait prendre la forme classique d’actes de colloque.
 							</p>
-
-
-							<p>Le matériau de base de notre colloque n’est donc pas constitué de textes mais de récits d’expériences autour d’une trame commune. <span class="texte__cache">Le pari du colloque fut que, sur cette base, via l’entrecroisement multiple de ces récits et de leur mise en perspective lors des moments forts du colloque (les débats publics (DP), les conférences plénières (PL), les tables de controverses), un nouvel horizon se lève sur les communs à même d’ouvrir la voie à une transformation globale de nos sociétés. Tout au long du colloque, pour pouvoir mieux cerner le translocalisme, il nous fallait réussir à identifier les liens, dans leur diversité, qui s’exprimeraient ou apparaîtraient dans cet entrecroisement de récits et de réflexions. Nous avons ainsi sollicité l’aide de personnes présentes sur toute la durée du colloque
-								en leur confiant un rôle de « capteurs de liens ».</span></p>
-
-							<p>Pour réfléchir aux contenus et à la forme de ces « anti-actes du colloque », nous nous sommes associés aux Éditions de l’Atelier et nous avons abouti à une architecture qui associe deux espaces, le livre papier (que nous
-								vous invitons à découvrir également) et ce site compagnon, reliés par un fil commun.</p>
-
-						<p><span class="texte__cache">En effet, chacun d’eux propose de raconter des voyages vers l’horizon translocaliste des territoires solidaires en commun. Quoique de manière différente.</span>
-							Dans le livre, nous proposons des récits de voyages et des poèmes de Jacques Jouet qui racontent comment, ces sept jours du 12 au 19 juillet 2019, ont fait événement. Chacun de ces récits renvoie à ce site internet compagnon,
-								qui vous offre la possibilité de construire votre propre voyage en traçant votre propre itinéraire singulier au pays du translocalisme des communs sur la base d’une architecture mettant en scène l’ensemble des matériaux
-							de ce colloque. </p>
-
-							<p>Vous pouvez suivre le <a href="<?= base_url("/timeline") ?>">déroulé chronologique du colloque</a> et faire votre choix d’escales parmi l’ensemble des tables rondes (TR) ou des débats publics (DP). Vous pouvez aussi suivre la pensée d’un·e intervenant·e
-								en particulier. Mais vous avez également la possibilité de partir à la découverte des <a href="<?= base_url("/thematiques") ?>">thématiques abordées</a><span class="texte__cache"> :
-									découvrir les moments où les quatre vecteurs du translocalisme que nous avions posés en hypothèse (la démocratie, la coopération, le numérique et l’écologie), ou bien de vous laisser porter par la matrice de questionnements, de liens et d’idées qui ont été repérés,
-								tout au long du colloque et dans les mois qui ont suivi, par les « capteurs de liens »,
-								les coorganisateurs et l’équipe des Éditions de l’Atelier. Ces liens et idées sont susceptibles de permettre à
-								chacun·e de construire ses chemins vers la société des communs.</span></p>
-
-							<p>Et partout, vous pourrez faire des rencontres et vous arrêter pour découvrir l’une des créations spontanées réalisées pendant le colloque
-							<span class="texte__cache"> (les poèmes, la chanson La Translocale, les annotations post-it, les étonnements, etc.)</span>
-								et manifestant la place essentielle du sensible dans les thèses abordées et les transformations recherchées.</p>
-
-							<p><span class="texte__cache">En cohérence avec l’approche des communs, ce site compagnon donne ainsi accès libre aux ressources produites par l’ensemble des participant·e·s avant, durant et après ce colloque.</span>
+							<p class="texte__cache">
+								Ce colloque s’est construit à partir de tables rondes (TR) dans lesquelles des actrices et des acteurs ont été invité·e·s à raconter quel territoire leurs actions collectives font localement émerger, comment celles-ci construisent des solidarités autour des ressources mises ou produites en commun et, enfin, quels liens les relient à d’autres échelles.
 							</p>
-							<p>Bon voyage et n’hésitez pas à nous en faire, à votre tour, le récit !</p>
+							<p>
+								Le matériau de base du colloque est en effet constitué de récits d’expériences.
+							</p>
+							<p class="texte__cache">
+								Le pari du colloque a été, pour mieux cerner le translocalisme, de favoriser l’entrecroisement multiple de ces récits et leur mise en perspective lors des moments forts du colloque (les débats publics [DP], les conférences plénières [PL], les tables de controverses). Pour identifier la diversité de ses liens qui s’exprimaient ou apparaissaient, nous avons sollicité l’aide de personnes présentes sur toute la durée du colloque en leur confiant un rôle de «capteurs de liens».
+							</p>
+							<p>
+								La proposition éditoriale associe deux espaces : le livre papier (que nous vous invitons à découvrir, publié par les Éditions de l’Atelier) et ce site compagnon.<br>
+								Chacun d’eux propose de raconter des voyages vers un horizon des communs à même d’ouvrir la voie à une transformation globale de nos sociétés.
+							</p>
+							<p>
+								Dans le livre, les organisateurs du colloque racontent comment ces sept jours du 12 au 19 juillet 2019, ont fait événement pour chacun d’entre eux.<br>
+								Le site vous offre, lui, la possibilité de construire votre propre voyage, de tracer votre propre itinéraire singulier vers la société des communs.
+
+							</p>
+							<p>
+								Vous pouvez suivre le <a href="<?= base_url("/timeline") ?>">déroulé chronologique</a> du colloque et faire votre choix d’escales parmi l’ensemble des tables rondes (TR) ou des débats publics (DP). Vous pouvez aussi suivre la pensée d’un·e <a href="<?= base_url("/intervenants") ?>">intervenant·e</a> en particulier. Mais vous avez également la possibilité de partir à la découverte des <a href="<?= base_url("/thematiques") ?>">thématiques abordées</a>
+								<span class="texte__cache">  : découvrir les moments où les quatre vecteurs du translocalisme posés en hypothèse (la démocratie, la coopération, le numérique et l’écologie), ou bien vous laisser porter par la matrice de questionnements, de liens et d’idées qui ont été repérés, tout au long du colloque et dans les mois qui ont suivi.</span>
+							</p>
+							<p>
+								Partout, vous pourrez faire des rencontres et vous arrêter pour découvrir l’une des créations spontanées réalisées pendant le colloque <span class="texte__cache"> (les poèmes, la chanson « La Translocale », les annotations post-it, les étonnements, etc.)</span> et manifestant la place essentielle du sensible dans les thèses abordées et les transformations recherchées.
+							</p>
+							<p class="texte__cache">
+								En cohérence avec l’approche des communs, ce site compagnon donne accès libre aux ressources produites par l’ensemble des participant·e·s avant, durant et après ce colloque.
+							</p>
+							<p>
+								N’hésitez pas à nous faire, à votre tour, le récit de votre propre voyage !
+							</p>
 
 						</div>
 
@@ -83,7 +83,7 @@
 		$(document).ready(function() {
 			$("button, .descriptif__livre").click(function() {
 				//$("span").toggleClass("texte__cache");
-				$("span.texte__cache").fadeToggle(1500);
+				$(".texte__cache").fadeToggle(1500);
 
 			})
 		})
--- application/views/thematiques.php
+++ application/views/thematiques.php
@@ -1,15 +1,171 @@
-<div class="container text-center pt-3">
-	<ul>
-	<?php
+<?php
+function startsWith ($string, $startString)
+{
+    $len = strlen($startString);
+    return (substr($string, 0, $len) === $startString);
+}
+?>
 
-	foreach ($thematiques as $key => $value)
-	{
-	?>
-		<li><a href="<?= base_url("/thematique/".$key) ?>"><?= $value ?></a></li>
-	<?php
-	}
-	?>
-	</ul>
+<div class="container text-center pt-3">
+	<div class="row">
+		<div class="col-12 my-4">
+			<h1 class="title h2 my-2 ml-2 mb-0 custom-title">Thématiques abordées</h1>
+		</div>
+	</div>
+	<div id="accordion" class="row">
+
+		<h5 class="mb-2 col-12">
+			<a href="<?= base_url("/thematique/01-elements-de-mode-demploi") ?>">
+				<button class="btn btn-primary col-12 col-md-6" data-toggle="" data-target="" aria-expanded="true" aria-controls="">
+				Éléments de mode d'emploi du colloque et du site compagnon
+				</button>
+			</a>
+		</h5>
+
+		<h5 class="mb-2 col-12">
+			<a href="<?= base_url("/thematique/02-ecoute-integrale") ?>">
+				<button class="btn btn-primary col-12 col-md-6" data-toggle="" data-target="" aria-expanded="false" aria-controls="">
+					Écoute intégrale des séances
+				</button>
+			</a>
+		</h5>
+
+
+		<div class="mb-2 col-12">
+			<div class="" id="heading3">
+				<h5 class="mb-0">
+					<button class="btn btn-primary collapsed col-12 col-md-6" data-toggle="collapse" data-target="#collapse3" aria-expanded="false" aria-controls="collapse3">
+						Liens avec le livre &#x25BC;
+					</button>
+				</h5>
+			</div>
+			<div id="collapse3" class="collapse" aria-labelledby="heading3" data-parent="#accordion">
+				<div class="card-body">
+						<?php
+
+						foreach ($thematiques as $key => $value)
+						{
+							if (startsWith($key, "1")) { ?>
+							<p><a href="<?= base_url("/thematique/".$key) ?>"><?= $value ?></a></p>
+							<?php }
+						}
+						?>
+				</div>
+			</div>
+		</div>
+
+
+		<h5 class="mb-2 col-12">
+			<a href="<?= base_url("/thematique/16poemes") ?>">
+				<button class="btn btn-primary col-12 col-md-6" data-toggle="" data-target="" aria-expanded="false" aria-controls="">
+					Poèmes de Jacques Jouet
+				</button>
+			</a>
+		</h5>
+
+		<div class="mb-2 col-12">
+			<div class="" id="heading5">
+				<h5 class="mb-0">
+					<button class="btn btn-primary collapsed col-12 col-md-6" data-toggle="collapse" data-target="#collapse5" aria-expanded="false" aria-controls="collapse5">
+						Les territoires solidaires &#x25BC;
+					</button>
+				</h5>
+			</div>
+			<div id="collapse5" class="collapse" aria-labelledby="heading5" data-parent="#accordion">
+				<div class="card-body">
+					<?php
+
+					foreach ($thematiques as $key => $value)
+					{
+						if (startsWith($key, "2")) { ?>
+							<p><a href="<?= base_url("/thematique/".$key) ?>"><?= $value ?></a></p>
+						<?php }
+					}
+					?>
+				</div>
+			</div>
+		</div>
+
+		<h5 class="mb-2 col-12">
+			<a href="<?= base_url("/thematique/31approchesdescommuns") ?>">
+				<button class="btn btn-primary col-12 col-md-6" data-toggle="" data-target="" aria-expanded="false" aria-controls="">
+				Approches des communs
+				</button>
+			</a>
+		</h5>
+
+		<h5 class="mb-2 col-12">
+			<a href="<?= base_url("/thematique/32approchesdelasolidarite") ?>">
+				<button class="btn btn-primary col-12 col-md-6" data-toggle="" data-target="" aria-expanded="false" aria-controls="">
+				Approches de la solidarité
+				</button>
+			</a>
+		</h5>
+
+		<h5 class="mb-2 col-12">
+			<a href="<?= base_url("/thematique/33translocalisme") ?>">
+				<button class="btn btn-primary col-12 col-md-6" data-toggle="" data-target="" aria-expanded="false" aria-controls="">
+				Approches du translocalisme
+				</button>
+			</a>
+		</h5>
+
+
+		<div class="mb-2 col-12">
+			<div class="" id="heading9">
+				<h5 class="mb-0">
+					<button class="btn btn-primary collapsed col-12 col-md-6" data-toggle="collapse" data-target="#collapse9" aria-expanded="false" aria-controls="collapse9">
+						Les vecteurs du translocalisme &#x25BC;
+					</button>
+				</h5>
+			</div>
+			<div id="collapse9" class="collapse" aria-labelledby="heading9" data-parent="#accordion">
+				<div class="card-body">
+					<?php
+
+					foreach ($thematiques as $key => $value)
+					{
+						if (startsWith($key, "4")) { ?>
+							<p><a href="<?= base_url("/thematique/".$key) ?>"><?= $value ?></a></p>
+						<?php }
+					}
+					?>
+				</div>
+			</div>
+		</div>
+
+		<div class="mb-2 col-12">
+			<div class="" id="heading10">
+				<h5 class="mb-0">
+					<button class="btn btn-primary collapsed col-12 col-md-6" data-toggle="collapse" data-target="#collapse10" aria-expanded="false" aria-controls="collapse10">
+						La Matrice des liens repérés pendant le colloque &#x25BC;
+					</button>
+				</h5>
+			</div>
+			<div id="collapse10" class="collapse" aria-labelledby="heading10" data-parent="#accordion">
+				<div class="card-body">
+					<?php
+
+					foreach ($thematiques as $key => $value)
+					{
+						if (startsWith($key, "5")) { ?>
+							<p><a href="<?= base_url("/thematique/".$key) ?>"><?= $value ?></a></p>
+						<?php }
+					}
+					?>
+				</div>
+			</div>
+		</div>
+
+
+		<h5 class="mb-2 col-12">
+			<a href="<?= base_url("/intervenants") ?>">
+				<button class="btn btn-primary col-12 col-md-6" data-toggle="" data-target="" aria-expanded="false" aria-controls="">
+				Les intervenants
+				</button>
+			</a>
+		</h5>
+	</div>
 </div>
 
 
